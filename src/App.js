82d7f98482f50d02557fe9fc58658d04c7f97402
jolanda <thelapegna@gmail.com>
2020-09-21 16:23:05 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <Form />
+      <List />
+      
+    
+    </div>
+  );
+}
+
+export default App;
